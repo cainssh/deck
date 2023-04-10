8e6a8f48657a224bba5db2ed8d72eb271a7bee24
Cain <39146511+cainssh@users.noreply.github.com>
2023-04-10 15:47:01 -0400

    Update
    consistent formatting, error handling, variables, functions

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,33 +1,60 @@
 #!/bin/bash
-# Package checking, Download and Install Multipass package
-pkg=`which multipass`
-if [ "$pkg" == "" ]; then
-    echo "Installing multipass, this will take a few secs ..."
-    # curl -L -C - https://github.com/canonical/multipass/releases/download/v1.8.1/multipass-1.8.1+mac-Darwin.pkg --output /tmp/multipass-1.8.1+mac-Darwin.pkg
-    sleep 5
-    sudo installer -pkg $1 -target /Applications
+PACKAGE_NAME="multipass"
+PACKAGE_URL="https://github.com/canonical/multipass/releases/download/v1.8.1/multipass-1.8.1+mac-Darwin.pkg"
+VM_NAME="deck-app"
+VM_CPUS=4
+VM_DISK_SIZE="10G"
+VM_MEMORY="4G"
+
+
+function install_package {
+    echo "Installing $PACKAGE_NAME, this will take a few seconds ..."
+    curl -L -C - $PACKAGE_URL --output /tmp/$PACKAGE_NAME.pkg
+    sudo installer -pkg /tmp/$PACKAGE_NAME.pkg -target /Applications
+}
+
+function create_vm {
+    echo "Creating a lightweight Ubuntu VM, this will take a few minutes ..."
+    multipass launch --name $VM_NAME -c $VM_CPUS -d $VM_DISK_SIZE -m $VM_MEMORY
+    multipass umount $VM_NAME
+}
+
+function configure_vm {
+    echo "Configuring VM settings ..."
+    multipass set client.primary-name=$VM_NAME
+    multipass set client.gui.autostart=false
+}
+
+function install_docker {
+    echo "Installing Docker on VM ..."
+    multipass exec $VM_NAME -- bash -c "curl https://raw.githubusercontent.com/deck-app/multipass-install/master/multipass-install.sh | sh"
+}
+
+function configure_nfs {
+    echo "Configuring NFS settings ..."
+    multipass exec $VM_NAME -- bash -c "mkdir -p /home/ubuntu/`whoami` && sudo touch /etc/auto.projects && 
+                                    sudo chown `multipass exec $VM_NAME whoami`:`multipass exec $VM_NAME whoami` /etc/auto.projects && 
+                                    echo /home/ubuntu/`whoami` -fstype=nfs,rw,nolock,nosuid,proto=tcp,resvport `ifconfig -l | xargs -n1 ipconfig getifaddr`:/Users/`whoami` | tee /etc/auto.projects"
+}
+
+
+if [ "$(which $PACKAGE_NAME)" == "" ]; then
+    install_package
 else
-    echo "Skipping download, Multipass is already installed"
+    echo "Skipping download, $PACKAGE_NAME is already installed."
 fi
-# Wait for multipass initialization
-while [ ! -S /var/run/multipass_socket ];
-do
+
+while [ ! -S /var/run/multipass_socket ]; do
     sleep 1
 done
-echo "ca7747dfcae5ee948ffee4c81099b1a4"
-echo "Creating a lightweight Ubuntu VM, this will take a few mins ..."
-# Create a virtual system(VM)
-multipass launch --name deck-app -c 4 -d 10G -m 4G
-multipass umount deck-app
-#Create directory
-# mkdir -p $HOME/deck-projects
-# multipass mount $HOME/deck-projects deck-app:/deck-projects
-# Set primary system(VM)
-multipass set client.primary-name=deck-app
-multipass set client.gui.autostart=false
-# Install docker in multipass virtual system(VM)
-echo "30cc5285d6bc297b9ca633042957def0"
-multipass exec deck-app -- bash -c "mkdir -p /home/ubuntu/`whoami` && sudo touch /etc/auto.projects && 
-                                    sudo chown `multipass exec deck-app whoami`:`multipass exec deck-app whoami` /etc/auto.projects && 
-                                    echo /home/ubuntu/`whoami` -fstype=nfs,rw,nolock,nosuid,proto=tcp,resvport `ifconfig -l | xargs -n1 ipconfig getifaddr`:/Users/`whoami` | tee /etc/auto.projects
-                                    curl https://raw.githubusercontent.com/deck-app/multipass-install/master/multipass-install.sh | sh"
+
+create_vm
+
+configure_vm
+
+install_docker
+
+configure_nfs
+
+# Done!
+echo "Script execution complete."
